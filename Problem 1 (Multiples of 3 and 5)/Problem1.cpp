4ed6d524b9c20c2e1a7e425104e10793076397d0
di <dimitar.rock@gmail.com>
2021-03-16 18:25:57 +0200

    cosmetic changes

--- Problem 1 (Multiples of 3 and 5)/Problem1.cpp
+++ Problem 1 (Multiples of 3 and 5)/Problem1.cpp
@@ -5,13 +5,11 @@ int multiplier(int num)
 	return 0;
 }
 
-
 template <typename ... M>
 int multiplier(int num, int m1, M... m)
 {
 	return num % m1 == 0 ? num : multiplier(num, m...);
 }
-
 
 template <typename ... M>
 int sumOfMultiples(int below, M... m)
@@ -30,8 +28,7 @@ int solve()
 	return sumOfMultiples(1000, 3, 5);
 }
 
-
 int main()
 {
-	std::cout << solve() << std::endl;
+	std::cout << solve() << "\n";
 }
