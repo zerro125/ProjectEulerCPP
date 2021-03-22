29247b18dfd622740933af2a4cf574ee25a6c21a
zerro125 <62221846+zerro125@users.noreply.github.com>
2021-03-22 12:06:28 +0200

    Problem 21

--- Problem 21 (Amicable numbers)/Problem21.cpp
+++ Problem 21 (Amicable numbers)/Problem21.cpp
@@ -1,10 +1,52 @@
 #include <iostream>
 
+bool amicable(int& a, int& b)
+{
+	int total = 0;
+	int result = 0;
 
+	for (size_t i = 1; i < a; i++)
+	{
+		if (a % i == 0)
+		{
+			total += i;
+		}
+	}
+
+	for (size_t j = 1; j < b; j++)
+	{
+		if (b % j == 0)
+		{
+			result += j;
+		}
+	}
+
+	if (total == b && result == a)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
 
 int solve()
 {
-	
+	int sum = 0;
+
+	for (int i = 1; i < 10001; i++)
+	{
+		for (int j = 1; j < 10001; j++)
+		{
+			if (amicable(i, j) == true && i != j)
+			{
+				sum += i + j;
+			}
+		}
+	}
+
+	return sum;
 }
 
 int main()
